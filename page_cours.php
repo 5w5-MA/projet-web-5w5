cb788d2602e518944d728bae6b647e1a54adb191
Crépeau <e2270083@cmaisonneuve.qc.ca>
2024-10-22 17:02:44 -0400

    debut page cours

---
+++ page_cours.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Template Name: cours
+ */
+?>
+
+<?php get_header(); ?>
+<main class="pageCours">
+    <section class="coursPrincipale">
+        <h1>Liste de cours</h1>
+        <div class="img"></div>
+        <div class="txt"></div>
+    </section>
+    <section class="coursMenu"></section>
+    <section class="listeCours"></section>
+</main>
+<?= get_footer(); ?>
