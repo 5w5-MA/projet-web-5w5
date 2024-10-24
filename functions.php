38aab5b70dea0665f6a9a2e48e6901889936e2f3
Alek Crépeau <118758017+Alek-Crepeau@users.noreply.github.com>
2024-10-23 22:52:30 -0400

    fin du menu de choix de session

--- functions.php
+++ functions.php
@@ -4,3 +4,10 @@ function files()
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'files');
+
+// function scriptJsCours()
+// {
+//     wp_enqueue_script('cours', get_template_directory_uri() . '/js/cours.js', array('jquery'), null, true);
+// }
+
+// add_action('wp_enqueue_scripts', 'scriptJsCours');
--- page_cours.php
+++ page_cours.php
@@ -19,24 +19,88 @@
         <input type="checkbox" id="chkBtnSession4">
         <input type="checkbox" id="chkBtnSession5">
         <input type="checkbox" id="chkBtnSession6">
-        <label for="chkBtnSession1" class="btnSession1">Session1</label>
-        <label for="chkBtnSession2" class="btnSession2">Session2</label>
-        <label for="chkBtnSession3" class="btnSession3">Session3</label>
-        <label for="chkBtnSession4" class="btnSession4">Session4</label>
-        <label for="chkBtnSession5" class="btnSession5">Session5</label>
-        <label for="chkBtnSession6" class="btnSession6">Session6</label>
+        <div class="btnSession1Wrap">
+            <label for="chkBtnSession1" class="btnSession1">Session1</label>
+        </div>
+        <div class="btnSession2Wrap">
+            <label for="chkBtnSession2" class="btnSession2">Session2</label>
+        </div>
+        <div class="btnSession3Wrap">
+            <label for="chkBtnSession3" class="btnSession3">Session3</label>
+        </div>
+        <div class="btnSession4Wrap">
+            <label for="chkBtnSession4" class="btnSession4">Session4</label>
+        </div>
+        <div class="btnSession5Wrap">
+            <label for="chkBtnSession5" class="btnSession5">Session5</label>
+        </div>
+        <div class="btnSession6Wrap">
+            <label for="chkBtnSession6" class="btnSession6">Session6</label>
+        </div>
     </section>
     <section class="listeCours"></section>
 </main>
 <?= get_footer(); ?>
-<!-- <script>
-    document
-        .getElementById("chkBurger")
-        .addEventListener("change", function() {
-            if (this.checked) {
-                document.body.classList.add("pasDeScroll");
+<script>
+    let btns = [{
+            btn: document.getElementsByClassName("btnSession1")[0],
+            wrap: document.getElementsByClassName("btnSession1Wrap")[0],
+            chk: document.getElementById("chkBtnSession1")
+        },
+        {
+            btn: document.getElementsByClassName("btnSession2")[0],
+            wrap: document.getElementsByClassName("btnSession2Wrap")[0],
+            chk: document.getElementById("chkBtnSession2")
+        },
+        {
+            btn: document.getElementsByClassName("btnSession3")[0],
+            wrap: document.getElementsByClassName("btnSession3Wrap")[0],
+            chk: document.getElementById("chkBtnSession3")
+        },
+        {
+            btn: document.getElementsByClassName("btnSession4")[0],
+            wrap: document.getElementsByClassName("btnSession4Wrap")[0],
+            chk: document.getElementById("chkBtnSession4")
+        },
+        {
+            btn: document.getElementsByClassName("btnSession5")[0],
+            wrap: document.getElementsByClassName("btnSession5Wrap")[0],
+            chk: document.getElementById("chkBtnSession5")
+        },
+        {
+            btn: document.getElementsByClassName("btnSession6")[0],
+            wrap: document.getElementsByClassName("btnSession6Wrap")[0],
+            chk: document.getElementById("chkBtnSession6")
+        }
+    ];
+
+    function gestionChangementCheckbox(changeChk) {
+        btns.forEach(({
+            btn,
+            wrap,
+            chk
+        }) => {
+            if (chk === changeChk) {
+                if (chk.checked) {
+                    btn.classList.add("actif");
+                    wrap.classList.add("wrapActif");
+                } else {
+                    btn.classList.remove("actif");
+                    wrap.classList.remove("wrapActif");
+                }
             } else {
-                document.body.classList.remove("pasDeScroll");
+                chk.checked = false;
+                btn.classList.remove("actif");
+                wrap.classList.remove("wrapActif");
             }
         });
-</script> -->
+    }
+
+    btns.forEach(({
+        chk
+    }) => {
+        chk.addEventListener("change", function() {
+            gestionChangementCheckbox(this);
+        });
+    });
+</script>
