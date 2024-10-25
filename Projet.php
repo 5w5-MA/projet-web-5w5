030484fb19531b50091d25c7bc78aa28e439e4ee
Max-J Rosalbert <rosalmjo@gmail.com>
2024-10-25 13:36:04 -0400

    Page Projet de Max Complété. Il manquerait seulement à vérifier l'intégration si c'est bien fait et s'il y a des modification à faire

---
+++ Projet.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+    <main class="MaxProjetMain">
+        <section class="SectionArcade">
+            <div class="TitreSectionArcade">
+                <div class="Partie1">
+                    <h2>Arcade</h2>
+                </div>
+                <div class="Partie2"></div>
+                <div class="Partie3"></div>
+            </div>
+            <section class="ListeArcade">
+                <div class="Jeu1"></div>
+                <div class="Jeu2"></div>
+                <div class="Jeu3"></div>
+                <div class="Jeu4"></div>
+                <div class="Jeu5"></div>
+                <div class="Jeu6"></div>
+            </section>
+        </section>
+        <section class="SectionProjet">
+            <section class="Rangee1">
+                <div class="FiltreSectionProjet"><div class="Partie1">
+                    <h2>Filtre</h2>
+                </div>
+                <div class="Partie2"></div>
+                <div class="Partie3"></div></div>
+
+                <div class="TitreSectionProjet">
+                    <div class="Partie1">
+                        <h2>Projet</h2>
+                    </div>
+                    <div class="Partie2"></div>
+                    <div class="Partie3"></div>
+                </div>
+            </section>
+
+            <div class="Rangee2">
+                <div class="div1"></div>
+                <div class="div2"></div>
+                <div class="div3"></div>
+                <div class="div4"></div>
+                <div class="div5"></div>
+                <div class="div6"></div>
+            </div>
+        </section>
+    </main>
+<?= get_footer(); ?>
