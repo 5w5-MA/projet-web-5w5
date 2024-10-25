2c75912a2ad077e7d3895afd16e735eef3c2d9e9
Christopher Simeus <e1639966@cmaisonneuve.qc.ca>
2024-10-25 14:05:02 -0400

    Ajout ficher Futur (mediaquery Incomplet)

---
+++ future.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+    <main>
+        <section class="intro_dessus">
+        
+            <h1>Possibilités d'Avenir</h1>
+        
+            <img class="" src="../images/Journal-IntegrAL-1200x642.jpg" alt="">
+
+        </section> 
+
+        <section class="intro">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit aliquid pariatur facilis eveniet accusantium sequi non quae, fugit ipsum veritatis eligendi iure modi a voluptas cum at quos ducimus numquam.</p>
+        </section>
+
+        <section class="options">
+            <div class="option1">Université</div>
+            <div class="option2">Emploi</div>
+            <div class="option3">Stage</div>
+        </section>
+
+        <section class="vide">
+            image
+        </section>
+
+        <section class="content">
+            <p>IMAGE IMAGE IMAGE</p>
+        </section>
+    </main>
+    <?= get_footer(); ?>
