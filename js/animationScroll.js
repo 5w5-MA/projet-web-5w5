5b4ac5ae9ca628dc3440e5e22fe07dab582b501f
MaggieD20 <coucoumaggie@hotmail.com>
2024-10-29 16:19:13 -0400

    essaie de scroll

---
+++ js/animationScroll.js
@@ -0,0 +1,24 @@
+
+
+// let lesImages = document.querySelectorAll(".imageVie");
+// console.log(lesImages)
+
+// window.addEventListener('scroll', ApparitionImg());
+
+// function ApparitionImg() {
+//     let bas = window.innerHeight / 3;
+//     console.log(bas);
+
+//     lesImages.forEach(img => {
+//         let hautImg = img.getBoundingClientRect().top;
+//         console.log(hautImg);
+//         if (hautImg < bas) {
+//             img.classList.add("apparitionImg");
+
+//         } else {
+//             img.classList.remove(".apparitionImg");
+//         }
+
+//     });
+// }
+
