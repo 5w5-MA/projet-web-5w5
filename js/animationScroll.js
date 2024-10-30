ad8f9cc68c376558416fe22dc485f74e5abd3539
MaggieD20 <coucoumaggie@hotmail.com>
2024-10-30 14:28:14 -0400

    javascript fonctionne

--- js/animationScroll.js
+++ js/animationScroll.js
@@ -1,24 +1,24 @@
 
 
-// let lesImages = document.querySelectorAll(".imageVie");
-// console.log(lesImages)
+let lesImages = document.querySelectorAll(".imageVie");
+console.log(lesImages)
 
-// window.addEventListener('scroll', ApparitionImg());
+window.addEventListener('scroll', ApparitionImg);
 
-// function ApparitionImg() {
-//     let bas = window.innerHeight / 3;
-//     console.log(bas);
+function ApparitionImg() {
+    let bas = window.innerHeight / 3;
+    console.log(bas);
 
-//     lesImages.forEach(img => {
-//         let hautImg = img.getBoundingClientRect().top;
-//         console.log(hautImg);
-//         if (hautImg < bas) {
-//             img.classList.add("apparitionImg");
+    lesImages.forEach(img => {
+        let hautImg = img.getBoundingClientRect().top;
+        console.log(bas);
+        if (hautImg < bas) {
+            img.classList.add("apparitionImg");
 
-//         } else {
-//             img.classList.remove(".apparitionImg");
-//         }
+        } else {
+            img.classList.remove("apparitionImg");
+        }
 
-//     });
-// }
+    });
+}
 
