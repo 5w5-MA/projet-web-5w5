ef01a79c699966ee29b6138e6f9dfae2abd6738e
Max-J Rosalbert <rosalmjo@gmail.com>
2024-11-01 17:31:48 -0400

    Ajout de mon code Projet complété

--- Projet.php
+++ Projet.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-    <main class="MaxProjetMain">
+<main class="MaxProjetMain">
         <section class="SectionArcade">
             <div class="TitreSectionArcade">
                 <div class="Partie1">
@@ -10,13 +10,32 @@
                 <div class="Partie3"></div>
             </div>
             <section class="ListeArcade">
-                <div class="Jeu1"></div>
-                <div class="Jeu2"></div>
-                <div class="Jeu3"></div>
-                <div class="Jeu4"></div>
-                <div class="Jeu5"></div>
-                <div class="Jeu6"></div>
+                <div class=" BoxProjet Jeu1"></div>
+                <div class=" BoxProjet Jeu2"></div>
+                <div class=" BoxProjet Jeu3"></div>
+                <div class=" BoxProjet Jeu4"></div>
+                <div class=" BoxProjet Jeu5"></div>
+                <div class=" BoxProjet Jeu6"></div>
             </section>
+            <!-- <section class="BoxFermer">
+                <article class="CarteJeu">
+                    <h3 class="TitreTp">Titre</h3>
+                    <div class="JeuBoite"></div>
+                    <h4 class="CoursCarte">Nom Cours</h4>
+                    <ul class="LogicielsCarte">
+                        <li class="Applicase"></li>
+                        <li class="Applicase"></li>
+                        <li class="Applicase"></li>
+                        <li class="Applicase"></li>
+                        <li class="Applicase"></li>
+                    </ul>
+                    <ul class="NomElevesCarte">
+                        <h4>Nom Élèves</h4>
+                        <li>Nom...</li>
+                    </ul>
+                    <p class="Description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti enim possimus officiis cupiditate debitis, sapiente minima, sit mollitia quia ab eaque nemo animi magni nesciunt porro minus nobis facilis? Exercitationem atque recusandae earum possimus iusto sed deserunt alias ratione hic. Provident placeat ratione numquam fugit quae modi. Laborum, blanditiis? Beatae.</p>
+                </article>
+            </section> -->
         </section>
         <section class="SectionProjet">
             <section class="Rangee1">
@@ -36,12 +55,12 @@
             </section>
 
             <div class="Rangee2">
-                <div class="div1"></div>
-                <div class="div2"></div>
-                <div class="div3"></div>
-                <div class="div4"></div>
-                <div class="div5"></div>
-                <div class="div6"></div>
+                <div class=" BoxProjet div1"></div>
+                <div class=" BoxProjet div2"></div>
+                <div class=" BoxProjet div3"></div>
+                <div class=" BoxProjet div4"></div>
+                <div class=" BoxProjet div5"></div>
+                <div class=" BoxProjet div6"></div>
             </div>
         </section>
     </main>
