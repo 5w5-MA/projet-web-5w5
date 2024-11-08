01a7823ab301bfd0c42cb989850fce326667ec96
Max-J Rosalbert <rosalmjo@gmail.com>
2024-11-08 14:44:14 -0500

    Ajout de la page 1 Projet de max

---
+++ UnProjet.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+        <main class="MaxUnProjetMain">
+            
+            <div class="TitreUnProjet">
+                    <div class="Partie1">
+                        <h2> Titre 1 Projet</h2>
+                    </div>
+                    <div class="Partie2"></div>
+                    <div class="Partie3"></div>
+            </div>
+
+            <section class="CarteJeuOmbreP">
+                <div class="CarteJeuBiso">
+                    <article class="BoiteCarte">
+                        <div class="MiniTitreCarte">
+                            <div class="Partie1">
+                                <h2> Titre </h2>
+                            </div>
+                            <div class="Partie2"></div>
+                            <div class="Partie3"></div>
+                        </div>
+                        <div class="JeuBoite"></div>
+                        <h4 class="CoursCarte">Nom Cours : </h4>
+                        <ul class="LogicielsCarte">
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                            <li class="Applicase"></li>
+                        </ul>
+                        <ul class="NomElevesCarte">
+                            <h4 class="TitreNomEleves">Nom Élèves :</h4>         
+                            <li>Nom...</li>
+                        </ul>
+                        <p class="Description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti enim possimus officiis cupiditate debitis, sapiente minima, sit mollitia quia ab eaque nemo animi magni nesciunt porro minus nobis facilis? Exercitationem atque recusandae earum possimus iusto sed deserunt alias ratione hic. Provident placeat ratione numquam fugit quae modi. Laborum, blanditiis? Beatae.</p>
+                    </article>
+                </div>
+            </section>            
+
+
+        </main>
+<?= get_footer(); ?>
