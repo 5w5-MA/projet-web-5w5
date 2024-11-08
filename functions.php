51c362d2183b506cabad01584d87febacb4554f2
annabellecard3nas <119866562+annabellecard3nas@users.noreply.github.com>
2024-11-08 15:09:09 -0500

    integration des pages de vie etudiante et cours

--- functions.php
+++ functions.php
@@ -4,6 +4,11 @@
 function files()
 {
     wp_enqueue_script('burgerScript', get_theme_file_uri('/js/burger.js'),array(),NULL,true);
+    wp_enqueue_script('coursScript',get_theme_file_uri('/js/cours.js'),array(),NULL,true);
+    wp_enqueue_script('buttonCoursScript', get_theme_file_uri('/js/buttonCours.js'),array(),NULL,true );
+    wp_enqueue_script('animationScrollVieEtudiante', get_theme_file_uri('js/animationScrollVieEtudiante.js'),array(),NULL, true);
+
+    wp_enqueue_media('lato','//fonts.googleapis.com/css2?family=Lato');
     wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'files');
@@ -22,5 +27,9 @@ function features(){
     register_nav_menu('headerMenuLocation', 'header menu location');
     register_nav_menu('footerMenuLocation', 'footer menu location');
     add_theme_support('title-tag');
+
+
+
+    add_image_size('imagesVieEtude', 1000,800,true);
 }
 add_action('after_setup_theme',  'features');
--- header.php
+++ header.php
@@ -4,14 +4,10 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Lato">
-    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/normalize.css" />
-    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css" />
     <?php wp_head(); ?>
 </head>
 
+<!-- ///////////////// HAUT DE PAGE /////////////////// -->
 <body>
     <header>
 
