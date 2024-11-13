3e76c43f59fe5ee7f269ee1f5e4cde0a45b59bdd
MaggieD20 <coucoumaggie@hotmail.com>
2024-11-13 13:54:22 -0500

    changement des titre pour avoir le gabarit

--- page-projets.php
+++ page-projets.php
@@ -1,31 +1,29 @@
 <?php get_header(); ?>
 
-    <main class="MaxProjetMain">
+<main class="MaxProjetMain">
 
     <section class="SectionArcade">
-            <div class="TitreSectionArcade">
-                <div class="Partie1">
-                    <h2>Arcade</h2>
-                </div>
-                <div class="Partie2"></div>
-                <div class="Partie3"></div>
+        <div class="TitreSectionArcade">
+            <div class="titreSection">
+                <h2>Arcade</h2>
             </div>
-        
+        </div>
+
         <?php
-        $arcadePost = new WP_Query(array( 
-            'posts_per_page'=> 6,
+        $arcadePost = new WP_Query(array(
+            'posts_per_page' => 6,
             'category_name' => 'arcade'
         )); ?>
-        
+
         <section class="ListeArcade">
             <?php
-            while($arcadePost ->have_posts()){
-                $arcadePost ->the_post();
+            while ($arcadePost->have_posts()) {
+                $arcadePost->the_post();
             ?>
                 <section class="jeu">
                     <div>
                         <?php the_title(); ?>
-                    </div>    
+                    </div>
                 </section>
             <?php }; ?>
         </section>
@@ -33,20 +31,17 @@
         <section class="SectionProjet">
             <section class="Rangee1">
                 <div class="FiltreSectionProjet">
-                    <div class="Partie1">
+                    <div class="titreSection">
                         <h2>Projet</h2>
                     </div>
                 </div>
-                <div class="Partie2"></div>
-                <div class="Partie3"></div></div>
+                </div>
 
                 <div class="TitreSectionProjet">
                     <div class="Partie1">
                         <h2>Filtre</h2>
                         <div class="boutonFiltreProjet"> - </div>
                     </div>
-                    <div class="Partie2"></div>
-                    <div class="Partie3"></div>
                 </div>
             </section>
 
@@ -60,32 +55,32 @@
 
             <div class="Rangee2">
                 <?php
-                    $postDesign = new WP_Query(array(
-                        'category_name' => 'design'
-                    ));
+                $postDesign = new WP_Query(array(
+                    'category_name' => 'design'
+                ));
 
-                    while($postDesign ->have_posts()){
-                        $postDesign ->the_post();
-                        ?>
-                        <div class="design">
-                            <?php the_title();?>
-                        </div>
-                        <?
-                    }
+                while ($postDesign->have_posts()) {
+                    $postDesign->the_post();
+                ?>
+                    <div class="design">
+                        <?php the_title(); ?>
+                    </div>
+                <?
+                }
 
-                    $postDesign = new WP_Query(array(
-                        'category_name' => 'programmation'
-                    ));
+                $postDesign = new WP_Query(array(
+                    'category_name' => 'programmation'
+                ));
 
-                    while($postDesign ->have_posts()){
-                        $postDesign ->the_post();
-                        ?>
-                        <div class="programmation">
-                            <?php the_title();?>
-                        </div>
-                        <?
-                    }
-                ?>   
+                while ($postDesign->have_posts()) {
+                    $postDesign->the_post();
+                ?>
+                    <div class="programmation">
+                        <?php the_title(); ?>
+                    </div>
+                <?
+                }
+                ?>
                 <div class="div1"></div>
                 <div class="div2"></div>
                 <div class="div3"></div>
@@ -94,5 +89,5 @@
                 <div class="div6"></div>
             </div>
         </section>
-    </main>
+</main>
 <?php get_footer(); ?>
