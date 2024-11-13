de46eea1ddb722b0c8778795043c8e7a06202eaf
Crépeau <e2270083@cmaisonneuve.qc.ca>
2024-11-13 14:43:26 -0500

    modification pour background

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 function files()
 {
     wp_enqueue_style('style', get_stylesheet_uri());
+    wp_enqueue_script('animationBackground', get_theme_file_uri('/js/animationBackground.js'), array(), NULL, true);
 }
 add_action('wp_enqueue_scripts', 'files');
 
