4b2d10be0746758afe2123e9e53faf94b73abb83
Crépeau <e2270083@cmaisonneuve.qc.ca>
2024-11-13 15:58:14 -0500

    modification du fichier fomctions pour permetre l'ajout de shortcode dans le menu wordpress

--- functions.php
+++ functions.php
@@ -6,6 +6,22 @@ function files()
 }
 add_action('wp_enqueue_scripts', 'files');
 
+// if (! has_filter('wp_nav_menu', 'do_shortcode')) {
+//     add_filter('wp_nav_menu', 'shortcode_unatop');
+//     add_filter('wp_nav_menu', 'do_shortcode', 11);
+// }
+
+function do_shortcode_in_menu($items, $args)
+{
+    foreach ($items as $item) {
+        // Applique le shortcode à chaque élément de menu
+        $item->title = do_shortcode($item->title);
+    }
+    return $items;
+}
+
+add_filter('wp_nav_menu_objects', 'do_shortcode_in_menu', 10, 2);
+
 // function scriptJsCours()
 // {
 //     wp_enqueue_script('cours', get_template_directory_uri() . '/js/cours.js', array('jquery'), null, true);
