34a0abffab0f007f602aa0a4d671cbb4846523cc
Alek Crépeau <118758017+Alek-Crepeau@users.noreply.github.com>
2024-11-13 21:42:32 -0500

    1

--- page-projets.php
+++ page-projets.php
@@ -1,31 +1,35 @@
 <?php get_header(); ?>
-
-    <main class="MaxProjetMain">
+<div id="bg">
+    <canvas id="canvas1"></canvas>
+    <canvas></canvas>
+    <canvas></canvas>
+</div>
+<main class="MaxProjetMain">
 
     <section class="SectionArcade">
-            <div class="TitreSectionArcade">
-                <div class="Partie1">
-                    <h2>Arcade</h2>
-                </div>
-                <div class="Partie2"></div>
-                <div class="Partie3"></div>
+        <div class="TitreSectionArcade">
+            <div class="Partie1">
+                <h2>Arcade</h2>
             </div>
-        
+            <div class="Partie2"></div>
+            <div class="Partie3"></div>
+        </div>
+
         <?php
-        $arcadePost = new WP_Query(array( 
-            'posts_per_page'=> 6,
+        $arcadePost = new WP_Query(array(
+            'posts_per_page' => 6,
             'category_name' => 'arcade'
         )); ?>
-        
+
         <section class="ListeArcade">
             <?php
-            while($arcadePost ->have_posts()){
-                $arcadePost ->the_post();
+            while ($arcadePost->have_posts()) {
+                $arcadePost->the_post();
             ?>
                 <section class="jeu">
                     <div>
                         <?php the_title(); ?>
-                    </div>    
+                    </div>
                 </section>
             <?php }; ?>
         </section>
@@ -38,7 +42,8 @@
                     </div>
                 </div>
                 <div class="Partie2"></div>
-                <div class="Partie3"></div></div>
+                <div class="Partie3"></div>
+                </div>
 
                 <div class="TitreSectionProjet">
                     <div class="Partie1">
@@ -60,58 +65,58 @@
 
             <div class="Rangee2">
                 <?php
-                    $postDesign = new WP_Query(array(
-                        'category_name' => 'design',
-                    ));
+                $postDesign = new WP_Query(array(
+                    'category_name' => 'design',
+                ));
 
-                    while($postDesign ->have_posts()){
-                        $postDesign ->the_post();
-                        ?>
-                        <div class="design">
-                            <?php the_title();?>
-                        </div>
-                        <?
-                    }
+                while ($postDesign->have_posts()) {
+                    $postDesign->the_post();
+                ?>
+                    <div class="design">
+                        <?php the_title(); ?>
+                    </div>
+                <?
+                }
 
-                    $postProgrammation = new WP_Query(array(
-                        'category_name' => 'programmation'
-                    ));
+                $postProgrammation = new WP_Query(array(
+                    'category_name' => 'programmation'
+                ));
 
-                    while($postProgrammation ->have_posts()){
-                        $postProgrammation ->the_post();
-                        ?>
-                        <div class="programmation">
-                            <?php the_title();?>
-                        </div>
-                        <?
-                    }
-                    $postJeu = new WP_Query(array(
-                        'category_name' => 'jeu'
-                    ));
+                while ($postProgrammation->have_posts()) {
+                    $postProgrammation->the_post();
+                ?>
+                    <div class="programmation">
+                        <?php the_title(); ?>
+                    </div>
+                <?
+                }
+                $postJeu = new WP_Query(array(
+                    'category_name' => 'jeu'
+                ));
 
-                    while($postJeu ->have_posts()){
-                        $postJeu ->the_post();
-                        ?>
-                        <div class="jeu">
-                            <?php the_title();?>
-                        </div>
-                        <?
-                    }
-                    $postModelisation = new WP_Query(array(
-                        'category_name' => 'modelisation'
-                    ));
+                while ($postJeu->have_posts()) {
+                    $postJeu->the_post();
+                ?>
+                    <div class="jeu">
+                        <?php the_title(); ?>
+                    </div>
+                <?
+                }
+                $postModelisation = new WP_Query(array(
+                    'category_name' => 'modelisation'
+                ));
 
-                    while($postModelisation ->have_posts()){
-                        $postModelisation ->the_post();
-                        ?>
-                        <div class="modélisation">
-                            <?php the_title();?>
-                        </div>
-                        <?
-                    }
-                ?>   
+                while ($postModelisation->have_posts()) {
+                    $postModelisation->the_post();
+                ?>
+                    <div class="modélisation">
+                        <?php the_title(); ?>
+                    </div>
+                <?
+                }
+                ?>
 
             </div>
         </section>
-    </main>
+</main>
 <?php get_footer(); ?>
